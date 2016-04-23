efacf9177868936d0335ae04c285cc0630650c5a
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2016-04-23 21:09:58 +0300

    Course 3. Week 2. Angular Forms and Form Validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,7 @@ angular.module('confusionApp', [])
         $scope.tab = 1;
         $scope.filtText = '';
         $scope.showDetails = false;
-        
+
         $scope.dishes = [
             {
                 name: 'Uthapizza',
@@ -64,7 +64,41 @@ angular.module('confusionApp', [])
             return ($scope.tab === checkTab);
         };
 
-        $scope.toggleDetails = function() {
+        $scope.toggleDetails = function () {
             $scope.showDetails = !$scope.showDetails;
+        };
+    }])
+    .controller('ContactController', ['$scope', function ($scope) {
+
+        $scope.feedback = {
+            mychannel: "", firstName: "", lastName: "",
+            agree: false, email: ""
+        };
+
+        var channels = [{value: "tel", label: "Tel."}, {value: "Email", label: "Email"}];
+        $scope.channels = channels;
+
+        $scope.invalidChannelSelection = false;
+    }])
+
+    .controller('FeedbackController', ['$scope', function ($scope) {
+        $scope.sendFeedback = function () {
+            console.log($scope.feedback);
+            
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {
+                    mychannel: "", firstName: "", lastName: "",
+                    agree: false, email: ""
+                };
+                $scope.feedback.mychannel = "";
+
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            }
         };
     }]);
