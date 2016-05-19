f2a8b7fef7831bca9d74a809455cd105d8c042fe
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2016-05-19 22:50:12 +0300

    Course 3. Week 4.Client-Server Communication using $http

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,7 +7,13 @@ angular.module('confusionApp')
         $scope.filtText = '';
         $scope.showDetails = false;
 
-        $scope.dishes = menuFactory.getDishes();
+        $scope.dishes = {};
+        menuFactory.getDishes()
+            .then(
+            function (response) {
+                $scope.dishes = response.data;
+            }
+        );
 
         $scope.select = function (setTab) {
             $scope.tab = setTab;
@@ -70,9 +76,15 @@ angular.module('confusionApp')
     .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory',
         function ($scope, $stateParams, menuFactory) {
 
-        var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
-        $scope.dish = dish;
-    }])
+            $scope.dish = {};
+            menuFactory.getDish(parseInt($stateParams.id, 10))
+                .then(
+                function (response) {
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                }
+            );
+        }])
 
     .controller('DishCommentController', ['$scope', function ($scope) {
         $scope.newComment = {
@@ -96,14 +108,23 @@ angular.module('confusionApp')
     }])
 
 // implement the IndexController and About Controller here
-    
+
     .controller('IndexController', ['$scope', '$stateParams', 'menuFactory', 'corporateFactory',
         function ($scope, $stateParams, menuFactory, corporateFactory) {
-            $scope.dish = menuFactory.getDish(0);
+
+            $scope.dish = {};
+            menuFactory.getDish(0)
+                .then(
+                function (response) {
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                }
+            );
+
             $scope.promotion = menuFactory.getPromotion(0);
             $scope.leader = corporateFactory.getLeader(3);
         }])
-    
+
 
     .controller('AboutController', ['$scope', 'corporateFactory', function ($scope, corporateFactory) {
         $scope.leaders = corporateFactory.getLeaders();
