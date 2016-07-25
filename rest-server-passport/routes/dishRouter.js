0563f233e1da32aa0dd4c66f1b721f200f813874
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2016-07-25 21:53:14 +0300

    Course 5. Week 3. changed privileges: dishRouter revert changes

--- rest-server-passport/routes/dishRouter.js
+++ rest-server-passport/routes/dishRouter.js
@@ -16,7 +16,7 @@ dishRouter.route('/')
         });
     })
 
-    .post(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    .post(Verify.verifyOrdinaryUser, function (req, res, next) {
         Dishes.create(req.body, function (err, dish) {
             if (err) throw err;
             console.log('Dish created!');
@@ -29,7 +29,7 @@ dishRouter.route('/')
         });
     })
 
-    .delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    .delete(Verify.verifyOrdinaryUser, function (req, res, next) {
         Dishes.remove({}, function (err, resp) {
             if (err) throw err;
             res.json(resp);
@@ -37,14 +37,14 @@ dishRouter.route('/')
     });
 
 dishRouter.route('/:dishId')
-    .get(Verify.verifyOrdinaryUser, function (req, res, next) {
+    .get(function (req, res, next) {
         Dishes.findById(req.params.dishId, function (err, dish) {
             if (err) throw err;
             res.json(dish);
         });
     })
 
-    .put(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    .put(function (req, res, next) {
         Dishes.findByIdAndUpdate(req.params.dishId, {
             $set: req.body
         }, {
@@ -55,7 +55,7 @@ dishRouter.route('/:dishId')
         });
     })
 
-    .delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    .delete(function (req, res, next) {
         Dishes.findByIdAndRemove(req.params.dishId, function (err, resp) {
             if (err) throw err;
             res.json(resp);
