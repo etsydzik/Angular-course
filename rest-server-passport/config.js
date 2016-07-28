866fe4cb9fe18ace7abe8828eda4fa170b4d3066
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2016-07-28 22:07:52 +0300

    Course 5. Week 4. Using OAuth with Passport and Facebook

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '893190720785602',
+        clientSecret: '4f568c5b4fdceaa3cd83c0158d0be4a6',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 };
