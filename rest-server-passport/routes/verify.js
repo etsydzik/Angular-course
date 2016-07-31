319faa9de85ae7712e0e84a32677f169b0bceb8f
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2016-07-31 18:12:58 +0300

    Course 5. Week 3. Changed function verifyAdmin

--- rest-server-passport/routes/verify.js
+++ rest-server-passport/routes/verify.js
@@ -36,34 +36,13 @@ exports.verifyOrdinaryUser = function (req, res, next) {
 };
 
 exports.verifyAdmin = function (req, res, next) {
-    // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-    // decode token
-    if (token) {
-        // verifies secret and checks exp
-        jwt.verify(token, config.secretKey, function (err, decoded) {
-            if (err) {
-                var err = new Error('You are not authenticated!');
-                err.status = 401;
-                return next(err);
-            } else {
-                var admin = req.decoded._doc.admin;
-                console.log("admin = " + admin);
-                if (admin) {
-                    return next()
-                } else {
-                    // admin is false
-                    var err = new Error('You are not authorized to perform this operation!');
-                    err.status = 403;
-                    return next(err);
-                }
-            }
-        });
+    var admin = req.decoded._doc.admin;
+    console.log("admin = " + admin);
+    if (admin) {
+        return next()
     } else {
-        // if there is no token
-        // return an error
-        var err = new Error('No token provided!');
+        // admin is false
+        var err = new Error('You are not authorized to perform this operation!');
         err.status = 403;
         return next(err);
     }
